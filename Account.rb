2b9532031096abaa230c751db0d1529c24b766a3
manojkrishnaks <manojkrishna.k.s@gmail.com>
2013-08-03 20:53:09 +0530

    Created Deposit method

--- Account.rb
+++ Account.rb
@@ -10,11 +10,18 @@ Class AccountName
 		else
 			@Balance-=amount
 			puts "You have successfully withdrawn Rs."#{amount}
-			puts "your new current balance is Rs."#{@Balance}
+			puts "your current balance is Rs."#{@Balance}
 		end
 
 	end
  	def deposit_money(amount=0)
+		if amount<=0
+			puts "Reposit amount should be greater than 0"
+		else
+			@Balance-=amount
+			puts "You have successfully Deposited Rs."#{amount}
+			puts "your current balance is Rs."#{@Balance}
+		end
  	end
 	def enquire_balance()
 	end
